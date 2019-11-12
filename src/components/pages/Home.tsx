de18b4c1efc01b72f52529e16fb499b0717f472c
Mackenzie Browne <info@mackbrowne.com>
2019-11-12 23:16:45 +0100

    Get List of Photos to Work

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,18 @@
 // Framework
 import React from 'react';
+import useAxios from 'axios-hooks';
 
 // Components
 import { Container, Row, Col } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+
+type GalleryPhoto = {
+  farm: string,
+  server: string,
+  id: string,
+  secret: string,
+  title: string
+};
 
 /**
  * Homepage for the app!
@@ -11,11 +21,36 @@ import { Container, Row, Col } from 'react-bootstrap';
  * @returns {StatelessComponent} Stateless functional React component.
  */
 export default function Home() {
+  const BASE_URL = 'https://www.flickr.com/services/rest/?';
+  const URL_PARAMS = new URLSearchParams({
+    api_key: '3433323375a1c4cb9baa5d2ade6a9685',
+    gallery_id: '72157690638331410',
+    method: 'flickr.galleries.getPhotos',
+    format: 'json',
+    nojsoncallback: '1'
+  }).toString();
+
+  // Get Photo List
+  const [
+    { data: galleryData, loading: galleryLoading, error: galleryError }
+  ] = useAxios(`${BASE_URL}${URL_PARAMS}`);
+
+  if (galleryLoading) return <p>Loading...</p>;
+  if (galleryError) return <p>Error!</p>;
+
   return (
     <Container fluid>
       <Row>
         <Col>
-          <p>Home</p>
+          {galleryData.photos.photo.map(
+            ({ farm, server, id, secret, title }: GalleryPhoto) => (
+              <Image
+                key={id}
+                src={`https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`}
+                alt={title}
+              />
+            )
+          )}
         </Col>
       </Row>
     </Container>
