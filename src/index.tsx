7ccf00c1078f4d222dc24099c1d8312361c4a7ef
Mackenzie Browne <info@mackbrowne.com>
2020-03-17 08:50:48 +0100

    update typescript, add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
 import 'typeface-roboto';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Start App
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Google Anlytics
+const {
+  REACT_APP_FIREBASE_API_KEY: apiKey,
+  REACT_APP_FIREBASE_AUTH_DOMAIN: authDomain,
+  REACT_APP_FIREBASE_DATABASE_URL: databaseURL,
+  REACT_APP_FIREBASE_PROJECT_ID: projectId,
+  REACT_APP_FIREBASE_STORAGE_BUCKET: storageBucket,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
+  REACT_APP_FIREBASE_APP_ID: appId,
+  REACT_APP_FIREBASE_MEASUREMENT_ID: measurementId
+} = process.env;
+
+firebase.initializeApp({
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
+});
+firebase.analytics();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
