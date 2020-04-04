548fc5c06a8843c40759f73d9cfc31c27c024c74
Mackenzie Browne <info@mackbrowne.com>
2020-04-04 20:44:44 +0200

    Clean up css, add text animations and clean filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Container, Col, Fade } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Col, Fade, Navbar } from 'react-bootstrap';
 
-import './App.css';
 import { Title, TitleLabel, CenterRow } from './App.style.js';
 
-import swear from './utils/swear';
-import useIdea from './utils/useIdea';
+import AddSwear from './utils/AddSwear';
+import useIdea from './hooks/useIdea';
 
 export default function App() {
   const {
@@ -13,16 +12,48 @@ export default function App() {
     isLoading
   } = useIdea();
 
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShowFooter(true), 1500);
+  }, [isLoading]);
+
+  const clean = window.location.pathname === '/clean';
   return (
-    <Container>
-      <CenterRow>
-        <Fade in={!isLoading}>
-          <Col>
-            <Title>{swear(title)}</Title>
-            <TitleLabel>{label}</TitleLabel>
-          </Col>
-        </Fade>
-      </CenterRow>
-    </Container>
+    <>
+      <Container>
+        <CenterRow>
+          <Fade in={!isLoading}>
+            <Col>
+              <Title>
+                {clean ? (
+                  title
+                ) : (
+                  <AddSwear sentence={title} swear={'fucking'} />
+                )}
+              </Title>
+              <TitleLabel>{label}</TitleLabel>
+            </Col>
+          </Fade>
+        </CenterRow>
+      </Container>
+
+      <Fade in={showFooter}>
+        <Navbar fixed="bottom">
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              {clean ? (
+                <a href="/" style={{ color: '#da0404' }}>
+                  Dirty Version
+                </a>
+              ) : (
+                <a href="/clean">Clean Version</a>
+              )}
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+      </Fade>
+    </>
   );
 }
