cde0ccf7246c7aacc252d53702f3f4cc2e8a98fe
Mackenzie Browne <info@mackbrowne.com>
2020-05-17 23:16:56 +0200

    add visual testing

---
+++ cypress/integration/examples/example.spec.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+context('Example', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('Test', () => {
+    cy.get('body').snapshot();
+    cy.percySnapshot()
+  })
+})
