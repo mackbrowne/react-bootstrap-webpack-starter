f1dc08289e1584782d768d185972d7d0b757e9fe
Mackenzie Browne <mackbrowne@gmail.com>
2021-05-19 18:34:09 +0200

    added firestore db

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,54 @@ import { Button, Container, Row, Col, Form } from 'react-bootstrap';
 // import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 // import PageOne from './PageOne';
-import { useState, useRef } from 'react';
+// import { useState, useRef } from 'react';
 import { Headline } from './App.style';
-import ReCAPTCHA from "react-google-recaptcha";
-import { useForm } from "react-hook-form";
+// import ReCAPTCHA from 'react-google-recaptcha';
+import { useForm } from 'react-hook-form';
+import firebase from 'firebase/app';
 
 const { Group, Label, Control, Text, Check } = Form;
 
 type formValues = {
-  fullName: string,
-  email: string
-}
+  fullName: string;
+  email: string;
+  optIn: boolean;
+  timeStamp: Date;
+};
 
 export default function App() {
-  const reCapRef = useRef<ReCAPTCHA>();
+  // const reCapRef = useRef<ReCAPTCHA>();
   const { register, handleSubmit } = useForm<formValues>();
-  // const handleSubmit = (async (data) => {
-  //     const token = await reCapRef.current.executeAsync();
-  //     console.log('token:', token)
-  // })
-  console.log('process.env:', process.env) // CAPTCHA_KEY is undefined
+
+  console.log('process.env:', process.env); // CAPTCHA_KEY is undefined
+
+  const signUp = handleSubmit(async ({ fullName, email, optIn }) => {
+    try {
+      // const token = await reCapRef.current.executeAsync();
+      // console.log('token:', token);
+
+      const peopleCollection = firebase.firestore().collection('people');
+
+      await peopleCollection.add({
+        fullName,
+        email,
+        optIn,
+        timestamp: new Date(),
+      });
+
+      alert('Thank You!');
+    } catch ({ message }) {
+      console.error(message);
+    }
+  });
+
   return (
     <Container className="px-md-0 pt-5">
-      <ReCAPTCHA 
-        sitekey={process.env.REACT_APP_CAPTCHA_KEY} 
-        size="invisible" 
+      {/* <ReCAPTCHA
+        sitekey={process.env.REACT_APP_CAPTCHA_KEY}
+        size="invisible"
         ref={reCapRef}
-      />
+      /> */}
       <Row>
         <Col className="text-center">
           <h1>Help Us Welcome Boris.</h1>
@@ -43,31 +64,34 @@ export default function App() {
           </p>
           <p>Sadly, the first generation of explorers is passing away.</p>
           INPUT GROUP CAPTCHA
-          <Form onSubmit={handleSubmit(async (data) => {
-              const token = await reCapRef.current.executeAsync();
-              console.log('token:', token)
-          })}>
+          <Form
+            onSubmit={signUp}
+          >
             <Group controlId="formBasicInfo">
               <Row>
-                  <Form.Control 
-                    placeholder="Full Name" 
-                    {...register("fullName", { required: true })}
-                  />
+                <Form.Control
+                  placeholder="Full Name"
+                  {...register('fullName', { required: true })}
+                />
               </Row>
             </Group>
             <Group controlId="formBasicEmail">
               <Label>Email address</Label>
-              <Control 
-                type="email" 
-                placeholder="Enter email" 
-                {...register("email")}
+              <Control
+                type="email"
+                placeholder="Enter email"
+                {...register('email')}
               />
               <Text className="text-muted">
                 We&apos;ll never share your email with anyone else.
               </Text>
             </Group>
             <Group controlId="formBasicCheckbox">
-              <Check type="checkbox" label="Check me out" />
+              <Check
+                type="checkbox"
+                label="Check me out"
+                {...register('optIn')}
+              />
             </Group>
             <Button variant="dark" type="submit">
               Submit
