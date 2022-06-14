9eecdb3b721c9a5611f692640562b61a3b415d52
Mackenzie Browne <mackbrowne@Mackenzies-MacBook-Pro.local>
2022-06-14 11:58:33 +0200

    errors are fixed and code is compiling again

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from 'firebase/app';
 import 'firebase/analytics';
-import 'typeface-roboto';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
