f0625f75918b88e0e37aa5579f8d177a3bc9c7b7
Mack Browne <mack.browne@shopify.com>
2022-08-19 10:15:32 +0200

    update resume 2022 august

--- src/Resume/index.tsx
+++ src/Resume/index.tsx
@@ -31,60 +31,77 @@ export default function Resume() {
           <SubTitle>Experience</SubTitle>
           <ExperienceList>
             <Experience>
+              <Company name="Shopify" location="Amsterdam" />
+              <Position
+                title="Senior Engineer - Retail Expansion"
+                time="June 2022 - Present"
+                duties={[
+                  'Expand the Shopify POS system to more regions and tax jurisdictions',
+                  'Build solutions to improve Shopify retail experience all over the world',
+                  'Mobile application development with React Native',
+                  'Web Development With ReactJS',
+                  'Backend Development with Ruby on Rails',
+                ]}
+              />
+            </Experience>
+            <Experience>
               <Company name="Workiva" location="Amsterdam" />
               <Position
-                title="Senior Full Stack Engineer - Global Solutions"
+                title="Senior Engineer - Global Solutions"
                 time="October 2021 - June 2022"
                 duties={[
-                  'Work closely with Customers to discover new solutions and features that can benefit all',
-                  'Design, develop, test and deliver those solutions in a span of weeks',
-                  'Screen resumes and interview new employees to further grow the Europe engineering team',
-                  'Gather feedback from customers to further improve globalization',
+                  "Part of a small, fast-moving, customer-focused development team focused on implementing high-priority new features for the company's largest customers",
+                  'Work closely with Customers to discover new solutions and features that can benefit all users',
+                  'Influence development culture in positive ways and bring best practices from past experiences into Workiva',
+                  'Gather feedback from customers to further improve services for global customers',
+                  'Design, develop, test and deliver those solutions in a span of days/weeks',
+                  'Grow the Europe engineering team. Resume screenings and interviews',
                 ]}
               />
             </Experience>
             <Experience>
               <Company name="Nationale Nederlanden" location="Amsterdam" />
               <Position
-                title="Lead Mobile Developer - Architecture and Innovation"
+                title="Senior Engineer and Team Lead - Architecture and Innovation"
                 time="July 2020 - September 2021"
                 duties={[
-                  'Leadership in building a design system and shared component library in React Native',
-                  'Design and Develop a re-usable, standardised CI/CD pipeline for testing and deployment',
-                  'Perform technical and cultural interviews to grow our own and other teams across the company',
+                  'Enable, Improve and Educate development teams with new techniques and practices to establish a mature development culture and community across the company.',
+                  'Work across multiple product teams to improve quality, performance, testing and deployment by building optimised, re-usable solutions.',
+                  'Conduct interviews and other hiring practices for different teams. Focused on improving the overall development skills and abilities across the company.',
+                  'Team Lead in building a design system and shared component library in React Native.',
+                  'Team Lead on mobile development team building new features for an already-established app.',
+                  'Design and Implement a re-usable, standardised CI/CD pipeline for testing and deployment.',
                 ]}
               />
             </Experience>
             <Experience>
               <Company name="Elsevier" location="Amsterdam" />
               <Position
-                title="Senior Full Stack Developer - Scopus Core Team"
+                title="Senior Engineer - Scopus Core Team"
                 time="Feb 2020 - June 2020"
                 duties={[
-                  'Involvement in major web stack migration to micro services and micro front-end technologies',
-                  'Lead efforts to implement a shared design system for the brand',
-                  "Perform technical interviews to grow the team.  Mentoring and 1 on 1's to improve team skills",
+                  'Planned, Designed and Implemented a company-specific command line interface to improve development processes and save time for developers across the team.',
+                  'Primary Focus is involvement in major web stack migration.',
+                  "Mentoring and 1 on 1's for junior / intermediate developers.",
+                  'Performing interviews to grow the team.',
                 ]}
               />
             </Experience>
             <Experience>
-              <Company name="Prodigy" location="Toronto" />
+              <Company name="Prodigy Education" location="Toronto" />
               <Position
-                title="Lead Mobile Application Developer - Game"
+                title="Senior Engineer and Team Lead - Game"
                 time="Mar 2019 – Sep 2019"
                 duties={[
-                  'Improve mobile user experience and design a standardised method for mobile-specific features',
-                  'Lead project to design and build internal tools from feedback to improve mobile QA testing speed',
-                  'Design and build standardised techniques, automation pipelines for tests, builds and deployment',
-                  'Facilitate cross-team collaboration to improve the mobile experience in all parts of the product',
-                ]}
-              />
-              <Position
-                title="Senior Full Stack Developer - School Leader App"
-                time="Jun 2018 – Mar 2019"
-                duties={[
-                  'Develop several in-house tools to improve productivity including CI tools and Slack bots',
-                  'Perform lunch time tech talks. Train and mentor developers in 1 on 1 meetings',
+                  'Drastically improve the mobile app development and quality assurance experience by developing tools that improve the efficiency of internal teams.',
+                  'Improve user experience by building mobile-specific features as well as improving any mobile usability issues and bugs.',
+                  'Design and Implement and Configure automation pipelines for QA, builds and deployment.',
+                  'Facilitate cross-team collaboration to improve the mobile experience in all parts of the app.',
+                  'Code mentor for several junior developers in the company.',
+                  'Full Stack Development using React, Node and GraphQL ',
+                  'Mobile Development with React Native',
+                  'Develop new features and improve the Admin/Principal user experience.',
+                  'Develop several in-house tools to improve productivity and integrate Github and CI communication through slack.',
                 ]}
               />
             </Experience>
